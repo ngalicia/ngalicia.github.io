b72f78ec8486fb816a0310b869e7a1f8b4742c90
Nery Galicia <galicianery7@gmail.com>
2023-07-02 21:56:24 -0600

    Se agregaron certificados.

--- assets/js/certificados.js
+++ assets/js/certificados.js
@@ -1,6 +1,11 @@
 const certificados = [
 
   {
+    title: "Curso de Power BI Desktop",
+    link: "https://www.udemy.com/certificate/UC-4cf631c4-76a0-4e4a-8ccf-cda2e0295249/"
+  },
+
+  {
     title: "React Desde Cero",
     link: "https://www.udemy.com/certificate/UC-28531df7-d53f-4645-8579-3fecaf513bb4/"
   },
@@ -8,6 +13,16 @@ const certificados = [
   {
     title: "Angular Desde Cero",
     link: "https://www.udemy.com/certificate/UC-1fd2453b-ec99-4db5-9a06-b570deb2cce3/"
+  },
+
+  {
+    title: "Curso completo de Java",
+    link: "https://www.udemy.com/certificate/UC-2d85a581-3d9e-4ebd-9372-39b0074c515f/"
+  },
+
+  {
+    title: "Curso completo de Python 3",
+    link: "https://www.udemy.com/certificate/UC-7047eddf-5a3d-4088-910a-5f0d7eff42c4/"
   }
 
 ]
